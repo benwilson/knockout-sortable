f7c264a87e05f4e6bb147afc26d9f59069ac989c
Ben Wilson <ben@mainlight.com>
2025-10-13 14:23:18 -0700

    Change define/require to match RA

--- src/knockout-sortable.js
+++ src/knockout-sortable.js
@@ -1,14 +1,14 @@
 ;(function(factory) {
     if (typeof define === "function" && define.amd) {
         // AMD anonymous module
-        define(["knockout", "jquery", "jquery-ui/ui/widgets/sortable", "jquery-ui/ui/widgets/draggable", "jquery-ui/ui/widgets/droppable"], factory);
+        define(["ko", "jquery", "jqueryui/ui/widgets/sortable", "jqueryui/ui/widgets/draggable", "jquery-ui/ui/widgets/droppable"], factory);
     } else if (typeof require === "function" && typeof exports === "object" && typeof module === "object") {
         // CommonJS module
-        var ko = require("knockout"),
+        var ko = require("ko"),
             jQuery = require("jquery");
-        require("jquery-ui/ui/widgets/sortable");
-        require("jquery-ui/ui/widgets/draggable");
-        require("jquery-ui/ui/widgets/droppable");
+        require("jqueryui/ui/widgets/sortable");
+        require("jqueryui/ui/widgets/draggable");
+        require("jqueryui/ui/widgets/droppable");
         factory(ko, jQuery);
     } else {
         // No module loader (plain <script> tag) - put directly in global namespace
